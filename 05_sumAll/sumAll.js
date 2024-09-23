4092e0ced27f05d60e919c0403c68d3a7f25101a
raiaaa17 <rayanarahadiva@gmail.com>
2024-09-23 15:51:17 +0800

    Finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(start, finish) {
+    if (start < 0 || finish < 0 || Math.floor(start) !== start || Math.floor(finish) !== finish) return "ERROR"
+    if (start > finish) [start, finish] = [finish, start];
+    
+    let res = 0;
+    for (let i = start; i <= finish; i++) {
+        res += i;
+    };
+    return res;
 };
 
 // Do not edit below this line
