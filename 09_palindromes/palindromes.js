7235cdf8ecc05a5d85609d30b7c9fdcac9c0e778
raiaaa17 <rayanarahadiva@gmail.com>
2024-09-23 17:38:18 +0800

    Finish 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,13 @@
-const palindromes = function () {
+const palindromes = function (pal) {
+    pal = pal.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\s]/g, '');
+    pal = pal.toLowerCase()
+
+    res = ''
+    for (chr of pal) {
+        res = chr + res;
+    };
+
+    return pal === res
 
 };
 
