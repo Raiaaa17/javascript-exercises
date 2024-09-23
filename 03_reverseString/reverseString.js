6e181916569b35ccf7569223474d089af950ed3c
raiaaa17 <rayanarahadiva@gmail.com>
2024-09-23 15:00:43 +0800

    Finish 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(sentence) {
+    const sentenceArray = sentence.split("")
+    let res = []
+    for (chr of sentenceArray) {
+        res.unshift(chr)
+    }
+    return res.join("")
 };
 
 // Do not edit below this line
