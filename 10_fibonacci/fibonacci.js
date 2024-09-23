8f623c68784618c630b7c0e82a43ef64c9debd53
raiaaa17 <rayanarahadiva@gmail.com>
2024-09-23 19:57:55 +0800

    Finish 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n)
+    if (n < 0) return "OOPS";
+    if (n == 0) return 0;
+    if (n == 1) return 1;
 
+    let a = 0;
+    let b = 1;
+    let c;
+
+    for (let i = 2; i <= n; i++) {
+        c = a + b;
+        [a, b] = [b, c];
+    };
+
+    return c;
 };
 
 // Do not edit below this line
